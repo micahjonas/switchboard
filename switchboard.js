9f07c05ccb45958a2e287b91ee3fd676dd589c7f
Micha Schwendener <micha.schwendener@gmail.com>
2021-02-03 22:30:14 +0100

    Basic setup with microbundler

---
+++ switchboard.js
@@ -0,0 +1,37 @@
+import Trouter from 'trouter'
+
+export default class CFTrouter extends Trouter {
+  constructor(opts={}) {
+		super(opts)
+  }
+  
+  async handleRequest(request) {
+    const url = new URL(request.url)
+    const { handlers, params } = this.find(request.method, url.pathname)
+    try {
+      for await (const handle of handlers) {
+        const res = await handle(request, params)
+        if (res && res instanceof Response) {
+          return res
+        }
+      }
+      return new Response(JSON.stringify(e, null, ' '), {
+        status: 400,
+        statusText: 'No response generated',
+      })
+    } catch (e) {
+      return new Response(JSON.stringify(e, null, ' '), {
+        status: 500,
+        statusText: 'Worker failed',
+      })
+    }
+  }
+
+  sendJsonResponse(json) {
+    return new Response(JSON.stringify(json), {
+      headers: {
+        'content-type': 'text/html;charset=UTF-8',
+      },
+    })
+  }
+}
